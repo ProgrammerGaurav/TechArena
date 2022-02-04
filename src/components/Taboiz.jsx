da52837c337ef330b78070f20efee94a2ff114ee
Gaurav Gupta <gauravgupta14376@gmail.com>
2022-02-04 23:13:44 +0530

    added video

--- src/components/Taboiz.jsx
+++ src/components/Taboiz.jsx
@@ -1,27 +1,41 @@
-import React from "react";
-import Slider from "react-slick";
+import React, { useState, useRef, useEffect } from "react";
 
 const Taboiz = () => {
-	const settings = {
-		dots: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		infinite: true,
-		autoplay: true,
-		speed: 500,
-		autoplaySpeed: 2000,
+	const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+	const videoRef = useRef(null);
+	const playVideo = () => {
+		console.log(isVideoPlaying);
+		if (isVideoPlaying) {
+			videoRef.current.pause();
+		} else {
+			videoRef.current.play();
+		}
 	};
+	useEffect(() => {
+		videoRef.current.addEventListener("play", () => {
+			setIsVideoPlaying(true);
+		});
+		videoRef.current.addEventListener("pause", () => {
+			setIsVideoPlaying(false);
+		});
+		videoRef.current.play();
+	}, []);
 	return (
 		<div className="taboiz">
 			<div className="title">meet our team </div>
-			<Slider {...settings} className="slider">
-				{[...Array(7)].map((_, i) => (
-					<div className="img-container" key={i}>
-						<img src={`/assets/taboiz/${i + 1}.jpg`} alt="techarena" />
-					</div>
-				))}
-			</Slider>
+			<div className="video-container">
+				{!isVideoPlaying && <img src="/assets/play.svg" alt="" />}
+				<video
+					src="/assets/taprep.mp4"
+					type="video/mp4"
+					ref={videoRef}
+					// controls
+					onClick={(e) => {
+						playVideo();
+					}}
+				></video>
+			</div>
 		</div>
 	);
 };
